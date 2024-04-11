d30f9f01a41eef24920cfc99de2fc4fb641b2506
silentmass <juha.leukkunen@gmail.com>
2024-04-12 01:41:53 +0300

    Dependencies 4

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ source ./venv/bin/activate
 # Install the package
 python -m pip install .
 
+echo "Setup complete and the environment is activated."
+
 # Activate the virtual environment
 source ./venv/bin/activate
-
-echo "Setup complete and the environment is activated."
